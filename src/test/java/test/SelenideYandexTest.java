75cb25dead0d744108a12a0affd6a28c2e9665de
Sirozhiddin Zhumabaev <szhumabaev@fun.co>
2021-02-16 19:52:19 +0300

    added YandexTest

---
+++ src/test/java/test/SelenideYandexTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
+
+import org.junit.jupiter.api.Test;
+
+
+public class SelenideYandexTest {
+
+    @Test
+    void justFindSelenideInYandex() {
+        open("https://www.yandex.ru/");
+        $("#text").setValue("Selenide").pressEnter();
+        // asssert
+        $$(".main__center").last().shouldHave(text("selenide.org"));
+    }
+}
